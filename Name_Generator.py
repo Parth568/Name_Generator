a10d621678e84bd3a7b8991131c14a13db09c32e
Parth Chaudhari <95208429+Parth568@users.noreply.github.com>
2023-07-18 22:56:47 +0530

    Add files via upload

---
+++ Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the name Generator!")
+working_item = input("What do you want to become as professional?\n")
+thinking_item = input("which things you wants to do in near future?\n")
+print("Your Generator name will be " + working_item + " " + thinking_item)
